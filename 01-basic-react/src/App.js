1c59090af99ef292f3b897644c70a7b7f20e6d0d
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-05-22 17:31:45 +0500

    creating a react project using npx create-react-app

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,11 @@
+import MyFunc from './Script.js';
+
+function App() {
+  return (
+    <>
+    <MyFunc />
+    </>
+  );
+}
+
+export default App;
---
+++ 01-basic-react/src/Script.js
@@ -0,0 +1,15 @@
+function MyFunc() {
+    return (
+        <h3>Hello React</h3>
+    )
+}
+
+export default MyFunc;
+
+
+/*
+Best Practices while making components
+ - always start component names with uppercase letter
+ - filename should also start with uppercase letter for components
+ - use extensions .jsx for files using html, while JS only files re okay with .js, while vite forces .jsx extension
+*/
