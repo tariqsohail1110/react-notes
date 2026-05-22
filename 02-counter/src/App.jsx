bd3e392cda241c0ad2b123f1e4ae2d0d795dd3af
Muhammad Tariq <tariqsohail1110@gmail.com>
2026-05-22 19:02:57 +0500

    added conditions in the counter project

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -8,14 +8,18 @@ function App() {
 
   let [counter, setCounter /* setVariableName is a naming convention (a best practice)*/] = useState(15); 
   // it gives an array, the variables will be initialized with []
+  let [isFirstDisabled, setIsFirstDiasbled] = useState(false);
+  let [isSecDisabled, setIsSecDisabled] = useState(false);
 
   // let counter = 15;
 
   const addValue = () => {
+    if(counter + 1 === 20) {setIsFirstDiasbled(true)};
     setCounter(counter + 1);
   }
 
   const removeValue = () => {
+    if(counter - 1 === 0) {setIsSecDisabled(true)};
     setCounter(counter - 1);
   }
   return (
@@ -23,9 +27,11 @@ function App() {
       <h1>Hello World</h1>
       <h2>Counter Value: {counter}</h2>
       <button
+      disabled={isFirstDisabled}
       onClick={addValue}>Increase value {counter}</button>
       <br />
       <button
+      disabled={isSecDisabled}
       onClick={removeValue}>Decrease value {counter}</button>
       <p>footer: {counter}</p>
     </>
